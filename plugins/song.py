740537f1688f46928c52f03ce6cd12c8b7d6c219
kannan5678900 <85219809+kannan5678900@users.noreply.github.com>
2021-10-04 12:56:21 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -46,7 +46,7 @@ async def about(client, message):
        await message.reply("➪<b>Name</b> : ✫<i>Music Downloader</i>\n➪<b>Developer</b> : ✫[𝘗𝘦𝘵𝘦𝘳 𝘗𝘢𝘳𝘬𝘦𝘳](https://t.me/Peterparker6)\n➪<b>Language</b> : ✫<i>Python3</i>\n➪<b>Server</b> : ✫[𝘏𝘦𝘳𝘰𝘬𝘶](https://heroku.com/)\n➪<b>Source Code</b> : ✫[𝘊𝘭𝘪𝘤𝘬 𝘏𝘦𝘳𝘦](https://github.com/Avengers105/Music-Bot)",
     )
 
-@Client.on_message(filters.command(["song"]))
+@Client.on_message(filters.command(['song']))
 async def song(client, message, Message):
     query = ''
     for i in message.command[1:]:
