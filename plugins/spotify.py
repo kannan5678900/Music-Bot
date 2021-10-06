c396e9332f50dbef0f11e305b4f9c5b00136d1b3
kannan5678900 <85219809+kannan5678900@users.noreply.github.com>
2021-10-06 08:46:36 +0530

    Create spotify.py

---
+++ plugins/spotify.py
@@ -0,0 +1,69 @@
+from spotify import DOWNLOADMP3 as SONGDOWNLOADER
+import telepot
+import spotify
+import requests
+import threading
+import os
+
+def txtfinder(txt):
+    a = txt.find("https://open.spotify.com")
+    txt = txt[a:]
+    return txt
+
+def cantfind(chat_id):
+    bot.sendSticker(chat_id, 'CAACAgQAAxkBAAIBE2BLNclvKLFHC-grzNdOEXKGl6cLAALzAAMSp2oDSBk1Yo7wCGUeBA')
+    bot.sendMessage(chat_id, "can't find it")
+
+def cantfindone(chat_id):
+    bot.sendSticker(chat_id, 'CAACAgQAAxkBAAIFSWBF_m3GHUtZJxQzobvD_iWxYVClAAJuAgACh4hSOhXuVi2-7-xQHgQ')
+    bot.sendMessage(chat_id, "can't download one of them")
+
+def downloader(link,chat_id,type):
+    PLAYLIST = False
+    if type=='AL':
+        ITEMS = spotify.album(link)
+    elif type == 'AR':
+        ITEMS = spotify.artist(link)
+    elif type == 'PL':
+        ITEMS = spotify.playlist(link)
+        PLAYLIST = True
+    else:
+        ITEMS = []
+
+    MESSAGE = ""
+    COUNT = 0
+    for song in ITEMS:
+        if PLAYLIST:
+            song = song['track']
+        COUNT+=1
+        MESSAGE += f"{COUNT}. {song['name']}\n"
+    bot.sendMessage(chat_id, MESSAGE)
+    for song in ITEMS:
+        if PLAYLIST:
+            song = song['track']
+
+        try:
+            SONGDOWNLOADER(song['href'], chat_id)
+        except:
+            cantfindone(chat_id)
+
+
+def START(msg,chat_id):
+    print(f"{chat_id}:{msg}")
+    msglink = txtfinder(msg)
+    if msglink[:30]==('https://open.spotify.com/album') :
+        downloader(msg,chat_id,'AL')
+
+    elif msglink[:30]== ('https://open.spotify.com/track')  :
+        try:
+            SONGDOWNLOADER(msg, chat_id)
+        except:
+            bot.sendSticker(chat_id,
+                            'CAACAgQAAxkBAAIFSWBF_m3GHUtZJxQzobvD_iWxYVClAAJuAgACh4hSOhXuVi2-7-xQHgQ')
+            bot.sendMessage(chat_id, "can't download music")
+
+    elif msg[:33] == 'https://open.spotify.com/playlist':
+        downloader(msg,chat_id,'PL')
+
+    elif msglink[:31] == ('https://open.spotify.com/artist'):
+            downloader(msg,chat_id,'AR')
