eb001793960b583384710f22b5a9fd2654dab6d7
kannan5678900 <85219809+kannan5678900@users.noreply.github.com>
2021-10-07 08:25:28 +0530

    Update song2.py

--- plugins/song2.py
+++ plugins/song2.py
@@ -11,15 +11,15 @@ def txtfinder(txt):
     txt = txt[a:]
     return txt
 
-def cantfind(chat.id, client):
-    client.sendSticker(chat.id, 'CAACAgQAAxkBAAIBE2BLNclvKLFHC-grzNdOEXKGl6cLAALzAAMSp2oDSBk1Yo7wCGUeBA')
-    client.sendMessage(chat.id, "can't find it")
+def cantfind(chat_id, client):
+    client.sendSticker(chat_id, 'CAACAgQAAxkBAAIBE2BLNclvKLFHC-grzNdOEXKGl6cLAALzAAMSp2oDSBk1Yo7wCGUeBA')
+    client.sendMessage(chat_id, "can't find it")
 
-def cantfindone(chat.id, client):
-    client.sendSticker(chat.id, 'CAACAgQAAxkBAAIFSWBF_m3GHUtZJxQzobvD_iWxYVClAAJuAgACh4hSOhXuVi2-7-xQHgQ')
-    client.sendMessage(chat.id, "can't download one of them")
+def cantfindone(chat_id, client):
+    client.sendSticker(chat_id, 'CAACAgQAAxkBAAIFSWBF_m3GHUtZJxQzobvD_iWxYVClAAJuAgACh4hSOhXuVi2-7-xQHgQ')
+    client.sendMessage(chat_id, "can't download one of them")
 
-def downloader(link,chat.id,type):
+def downloader(link,chat_id,type):
     PLAYLIST = False
     if type=='AL':
         ITEMS = spotify.album(link)
@@ -44,31 +44,31 @@ def downloader(link,chat.id,type):
             song = song['track']
 
         try:
-            SONGDOWNLOADER(song['href'], chat.id)
+            SONGDOWNLOADER(song['href'], chat_id)
         except:
-            cantfindone(chat.id)
+            cantfindone(chat_id)
 
 
 @Client.on_message(filters.regex(pattern=".*https.* (.*)"))
 async def a(client,message):
           msglink = txtfinder(message)
           if msglink[:30]==('https://open.spotify.com/album') :
-              downloader(message,chat.id,'AL')
+              downloader(message,chat_id,'AL')
 
           elif msglink[:30]== ('https://open.spotify.com/track')  :
               try:
-                  SONGDOWNLOADER(message, chat.id)
+                  SONGDOWNLOADER(message, chat_id)
               except:
-                  client.sendSticker(chat.id,
+                  client.sendSticker(chat_id,
                                         'CAACAgQAAxkBAAIFSWBF_m3GHUtZJxQzobvD_iWxYVClAAJuAgACh4hSOhXuVi2-7-xQHgQ')
-                  message.reply_text(chat.id, "can't download music")
+                  message.reply_text(chat_id, "can't download music")
 
           elif msglink[:33] == 'https://open.spotify.com/playlist':
-                downloader(message,chat.id,'PL')
+                downloader(message,chat_id,'PL')
 
           elif msglink[:31] == ('https://open.spotify.com/artist'):
-                downloader(message,chat.id,'AR')
+                downloader(message,chat_id,'AR')
 
 
-print()
+print('{BOT_USERNAME} Started!')
 
